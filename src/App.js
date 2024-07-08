a95f23ac245051ac510317515ed5274c8207aab9
imanebelhajj <imanebelhaj292@gmail.com>
2024-07-08 14:13:23 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+// App.js
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Form from './composent/Form';
 import List from './composent/List';
 import Details from './composent/Details';
+import Home from './composent/Home';
 import './App.css';
-import './composent/Form.css';
+
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = (search) => {
+    console.log('Search term:', search); // Log the search term
     fetch(`https://api.themoviedb.org/3/search/tv?api_key=fef55a6754f2f6d00a0038388915039c&include_adult=false&query=${search}`)
       .then(response => response.json())
-      .then(data => setMovies(data.results))
+      .then(data => {
+        console.log('API response data:', data); // Log the API response
+        setMovies(data.results);
+      })
       .catch(error => console.error('Error fetching data:', error));
   };
 
@@ -29,16 +36,29 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <Form onSearch={handleSearch} />
-      {isLoading ? (
-        <div className="loading">Loading...</div>
-      ) : selectedMovie ? (
-        <Details movie={selectedMovie} onBack={handleBack} />
-      ) : (
-        <List movies={movies} onMovieClick={handleMovieClick} />
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <div className="navbar">
+          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/search" className="navbar-link">Search</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={
+            <>
+              <Form onSearch={handleSearch} />
+              {isLoading ? (
+                <div className="loading">Loading...</div>
+              ) : selectedMovie ? (
+                <Details movie={selectedMovie} onBack={handleBack} />
+              ) : (
+                <List movies={movies} onMovieClick={handleMovieClick} />
+              )}
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/composent/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-content">
+        <div className="description">
+          <h1>Welcome to the Movies App</h1>
+          <p>Search and explore your favorite movies and TV shows.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
